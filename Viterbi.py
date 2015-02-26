67548be6826aad1b354fda6b689f0c4131a0540f
stiff <a.e.stiff@gmail.com>
2015-02-26 16:23:41 -0500

    Implemented findShortestPath without back pointers

--- Viterbi.py
+++ Viterbi.py
@@ -21,11 +21,19 @@ class Viterbi:
     sentence is a list of word strings 
     '''
     def findShortestPath(self, sentence):
-        viterbi = np.zeros(len(self.tagIndex),len(sentence))
+        viterbi = np.zeros((len(sentence),len(self.tagIndex)))
+        for i in range(len(self.tagIndex)):
+            viterbi[0,i] = self.transitionProbs[i,self.tagIndex.index("start")]+ \
+                self.emissionProbs[self.wordIndex.index(sentence[0]),i]
+        for j in range(1,len(sentence)):
+            for i in range(len(self.tagIndex)):
+                viterbi[j,i] = min(viterbi[j-1]+self.transitionProbs[i]+self.emissionProbs[self.wordIndex.index(sentence[j])])
+        print(min(viterbi[len(viterbi)-1]))
+            
         
 
 if __name__ == '__main__':
-    tData = {("NN","NN"):2,("VB","NN"):1,("VB","VB"):2,("NN","VB"):3}
+    tData = {("NN","NN"):2,("VB","NN"):1,("VB","VB"):2,("NN","VB"):3, ("start","NN"):6,("start","VB"):3}
     eData = {("dog","NN"):5, ("bark","VB"):4}
     v = Viterbi(tData,eData)
-    print(v.emissionProbs)
+    print(v.findShortestPath(["dog", "bark"]))
