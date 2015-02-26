2825fc8c2d764b4ec30c07a6cd58aff0f36fadca
stiff <a.e.stiff@gmail.com>
2015-02-26 15:29:39 -0500

    Constructor done

--- Viterbi.py
+++ Viterbi.py
@@ -4,5 +4,28 @@ Created on Feb 26, 2015
 @author: stiff
 '''
 
+import numpy as np
+
+class Viterbi:
+    def __init__(self, transitionProbs, emissionProbs):
+        self.tagIndex = sorted(set([tag for (tag, otherTag) in transitionProbs.keys()]))
+        self.wordIndex = sorted(set([word for (word, tag) in emissionProbs.keys()]))    
+        self.transitionProbs = np.ndarray((len(self.tagIndex),len(self.tagIndex)))
+        for (tag, otherTag) in transitionProbs.keys():
+            self.transitionProbs[self.tagIndex.index(tag),self.tagIndex.index(otherTag)]=transitionProbs[(tag,otherTag)]
+        self.emissionProbs = np.zeros((len(self.wordIndex),len(self.tagIndex)))
+        for (word, tag) in emissionProbs.keys():
+            self.emissionProbs[self.wordIndex.index(word),self.tagIndex.index(tag)]=emissionProbs[(word,tag)]
+
+    ''' 
+    sentence is a list of word strings 
+    '''
+    def findShortestPath(self, sentence):
+        viterbi = np.zeros(len(self.tagIndex),len(sentence))
+        
+
 if __name__ == '__main__':
-    pass
+    tData = {("NN","NN"):2,("VB","NN"):1,("VB","VB"):2,("NN","VB"):3}
+    eData = {("dog","NN"):5, ("bark","VB"):4}
+    v = Viterbi(tData,eData)
+    print(v.emissionProbs)
